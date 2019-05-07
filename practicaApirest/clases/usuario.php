405514ac2ff7748a61c6465c38510e8b6dce23ad
Emiliano Rago <emiliano.rg93@gmail.com>
2019-05-07 03:40:40 -0300

    Alta / Traer uno / Traer todos

---
+++ practicaApirest/clases/usuario.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once "AccesoDatos.php";
+
+class usuario
+{
+    public $id;
+    public $usuario;
+    public $password;
+
+    public function InsertarElCdParametros()
+    {
+            $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+            $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into usuario (usuario,password)values(:usuario,:password)");
+            $consulta->bindValue(':usuario', $this->usuario, PDO::PARAM_STR);
+            $consulta->bindValue(':password', $this->password, PDO::PARAM_STR);
+            $consulta->execute();		
+            return $objetoAccesoDato->RetornarUltimoIdInsertado();
+    }
+
+    public static function TraerUnUsuario($id) 
+	{
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("select id, usuario , password from usuario where id = $id");
+			$consulta->execute();
+			$cdBuscado= $consulta->fetchObject('usuario');
+			return $cdBuscado;					
+    }
+    
+    public static function TraerTodoLosUsuarios()
+	{
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("select id,usuario,password from usuario");
+			$consulta->execute();			
+			return $consulta->fetchAll(PDO::FETCH_CLASS, "usuario");		
+	}
+
+}
+
+
+
+
+
+
+?>
